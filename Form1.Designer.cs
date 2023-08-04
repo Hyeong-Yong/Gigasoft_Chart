40ef05a4b407f358aa4bcddedd13c6e14fa60e8d
황형용 <hwanghyeongyoung@gmail.com>
2023-08-04 17:50:57 +0900

    TODO : Last queue data is being ignored(Count < index). Fix them

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace chart_example3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pesgo1 = new Gigasoft.ProEssentials.Pesgo();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // pesgo1
+            // 
+            pesgo1.Dock = DockStyle.Fill;
+            pesgo1.Location = new Point(0, 0);
+            pesgo1.Name = "pesgo1";
+            pesgo1.Size = new Size(800, 450);
+            pesgo1.TabIndex = 0;
+            pesgo1.Text = "pesgo1";
+            pesgo1.PeCustomTrackingDataText += pesgo1_PeCustomTrackingDataText;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Interval = 250;
+            timer2.Tick += timer2_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pesgo1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Gigasoft.ProEssentials.Pesgo pesgo1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
